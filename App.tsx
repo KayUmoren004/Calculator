1c45b4e58405b6523cdf08eb1f8de51c970bad02
Godson Umoren <umorenkaykay@gmail.com>
2021-01-06 20:31:30 -0500

    Style Setup

--- App.tsx
+++ App.tsx
@@ -1,21 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+
+     <View style={styles.answer_view}>
+        <Text style={styles.top_operators}></Text>
+     </View>
+
+     <View style={styles.first_column}>
+
+     </View>
+
+     <View style={styles.second_column}>
+
+     </View>
+
+     <View style={styles.third_column}>
+
+     </View>
+
+     <View style={styles.fourth_column}>
+
+     </View>
+
+     <View style={styles.fifth_column}>
+
+     </View>
+
+      <StatusBar style="light" />
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  answer_view: {
+
+  },
+  first_column: {
+
+  },
+  second_column: {
+
+  },
+  third_column: {
+
+  },
+  fourth_column: {
+
+  },
+  fifth_column: {
+
+  },
+  top_operators: {
+
   },
 });
