807f8d45ea3bde34aa8dd20a7645581e0a94278a
Godson Umoren <umorenkaykay@gmail.com>
2021-01-09 00:34:28 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,151 +1,145 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      <View style={styles.answer_view}>
+        <Text style={styles.answer_text}>0</Text>
+      </View>
 
-     <View style={styles.answer_view}>
-      <Text style={styles.answer_text}>0</Text>
-     </View>
+      <View style={styles.first_column}>
+        <TouchableOpacity>
+          <View style={styles.operators_view}>
+            <Text style={styles.operators}>AC</Text>
+          </View>
+        </TouchableOpacity>
 
-     <View style={styles.first_column}>
+        <TouchableOpacity>
+          <View style={styles.operators_view}>
+            <Text style={styles.operators}>+/-</Text>
+          </View>
+        </TouchableOpacity>
 
-       <TouchableOpacity>
-       <View style={styles. operators_view}>
-        <Text style={styles. operators}>AC</Text>
-       </View>
-       </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.operators_view}>
+            <Text style={styles.operators}>%</Text>
+          </View>
+        </TouchableOpacity>
 
-       <TouchableOpacity>
-       <View style={styles. operators_view}>
-        <Text style={styles. operators}>+/-</Text>
-       </View>
-       </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.operators_view}>
+            <Text style={styles.operators}>÷</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
 
-       <TouchableOpacity>
-       <View style={styles. operators_view}>
-        <Text style={styles. operators}>%</Text>
-       </View>
-       </TouchableOpacity>
+      <View style={styles.second_column}>
+        <TouchableOpacity>
+          <View style={styles.input_view}>
+            <Text style={styles.input}>7</Text>
+          </View>
+        </TouchableOpacity>
 
-       <TouchableOpacity>
-       <View style={styles. operators_view}>
-        <Text style={styles. operators}>÷</Text>
-       </View>
-       </TouchableOpacity>
-       
-     </View>
+        <TouchableOpacity>
+          <View style={styles.input_view}>
+            <Text style={styles.input}>8</Text>
+          </View>
+        </TouchableOpacity>
 
-     <View style={styles.second_column}>
+        <TouchableOpacity>
+          <View style={styles.input_view}>
+            <Text style={styles.input}>9</Text>
+          </View>
+        </TouchableOpacity>
 
-       <TouchableOpacity>
-       <View style={styles. input_view}>
-        <Text style={styles. input}>7</Text>
-       </View>
-       </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.operators_view}>
+            <Text style={styles.operators}>×</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
 
-       <TouchableOpacity>
-       <View style={styles. input_view}>
-        <Text style={styles. input}>8</Text>
-       </View>
-       </TouchableOpacity>
+      <View style={styles.third_column}>
+        <TouchableOpacity>
+          <View style={styles.input_view}>
+            <Text style={styles.input}>4</Text>
+          </View>
+        </TouchableOpacity>
 
-       <TouchableOpacity>
-       <View style={styles. input_view}>
-        <Text style={styles. input}>9</Text>
-       </View>
-       </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.input_view}>
+            <Text style={styles.input}>5</Text>
+          </View>
+        </TouchableOpacity>
 
-       <TouchableOpacity>
-       <View style={styles. operators_view}>
-        <Text style={styles. operators}>×</Text>
-       </View>
-       </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.input_view}>
+            <Text style={styles.input}>6</Text>
+          </View>
+        </TouchableOpacity>
 
-     </View>
+        <TouchableOpacity>
+          <View style={styles.operators_view}>
+            <Text style={styles.operators}>-</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
 
-     <View style={styles.third_column}>
+      <View style={styles.fourth_column}>
+        <TouchableOpacity>
+          <View style={styles.input_view}>
+            <Text style={styles.input}>1</Text>
+          </View>
+        </TouchableOpacity>
 
-       <TouchableOpacity>
-       <View style={styles. input_view}>
-        <Text style={styles. input}>4</Text>
-       </View>
-       </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.input_view}>
+            <Text style={styles.input}>2</Text>
+          </View>
+        </TouchableOpacity>
 
-       <TouchableOpacity>
-       <View style={styles. input_view}>
-        <Text style={styles. input}>5</Text>
-       </View>
-       </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.input_view}>
+            <Text style={styles.input}>3</Text>
+          </View>
+        </TouchableOpacity>
 
-       <TouchableOpacity>
-       <View style={styles. input_view}>
-        <Text style={styles. input}>6</Text>
-       </View>
-       </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.operators_view}>
+            <Text style={styles.operators}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
 
-       <TouchableOpacity>
-       <View style={styles. operators_view}>
-        <Text style={styles. operators}>-</Text>
-       </View>
-       </TouchableOpacity>
+      <View style={styles.fifth_column}>
+        <TouchableOpacity>
+          <View style={styles.input_view}>
+            <Text style={styles.input}>0</Text>
+          </View>
+        </TouchableOpacity>
 
-     </View>
+        <TouchableOpacity>
+          <View style={styles.input_view}>
+            <Text style={styles.input}>.</Text>
+          </View>
+        </TouchableOpacity>
 
-     <View style={styles.fourth_column}>
-
-       <TouchableOpacity>
-       <View style={styles. input_view}>
-        <Text style={styles. input}>1</Text>
-       </View>
-       </TouchableOpacity>
-
-       <TouchableOpacity>
-       <View style={styles. input_view}>
-        <Text style={styles. input}>2</Text>
-       </View>
-       </TouchableOpacity>
-
-       <TouchableOpacity>
-       <View style={styles. input_view}>
-        <Text style={styles. input}>3</Text>
-       </View>
-       </TouchableOpacity>
-
-       <TouchableOpacity>
-       <View style={styles. operators_view}>
-        <Text style={styles. operators}>+</Text>
-       </View>
-       </TouchableOpacity>
-
-     </View>
-
-     <View style={styles.fifth_column}>
-
-       <TouchableOpacity>
-       <View style={styles. input_view}>
-        <Text style={styles. input}>0</Text>
-       </View>
-       </TouchableOpacity>
-
-       <TouchableOpacity>
-       <View style={styles. input_view}>
-        <Text style={styles. input}>.</Text>
-       </View>
-       </TouchableOpacity>
-
-       <TouchableOpacity>
-       <View style={styles. operators_view}>
-        <Text style={styles. operators}>=</Text>
-       </View>
-       </TouchableOpacity>
-
-     </View>
+        <TouchableOpacity>
+          <View style={styles.operators_view}>
+            <Text style={styles.operators}>=</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
 
       <StatusBar style="light" />
-
     </SafeAreaView>
   );
 }
@@ -153,71 +147,69 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
     width: "100%",
   },
   answer_view: {
-    flexDirection:"row",
-    backgroundColor:"#000",
+    flexDirection: "row",
+    backgroundColor: "#000",
     width: "100%",
     //height: 100,
     //marginTop: 200,
     //justifyContent:"flex-end",
   },
-  answer_text:{
+  answer_text: {
     //textAlignVertical:"bottom",
-    color:"#fff",
+    color: "#fff",
     paddingTop: 100,
-    backgroundColor:"#000",
+    backgroundColor: "#000",
     width: "100%",
-    textAlign:'right',
-    fontWeight:"400",
-    fontSize:100
+    textAlign: "right",
+    fontWeight: "400",
+    fontSize: 100,
   },
   first_column: {
-    flexDirection:"row"
+    flexDirection: "row",
   },
   second_column: {
-    flexDirection:"row"
+    flexDirection: "row",
   },
   third_column: {
-    flexDirection:"row"
+    flexDirection: "row",
   },
   fourth_column: {
-    flexDirection:"row"
+    flexDirection: "row",
   },
   fifth_column: {
-    flexDirection:"row"
+    flexDirection: "row",
   },
-   operators: {
-    color:"#000",
-    textAlign:"center",
-    fontSize:30,
-    fontWeight:"bold"
-  
+  operators: {
+    color: "#000",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold",
   },
-   operators_view: {
+  operators_view: {
     width: 80,
     height: 80,
-    borderRadius: 80/2,
-    backgroundColor:"#aeaeae",
-    justifyContent:"center",
+    borderRadius: 80 / 2,
+    backgroundColor: "#aeaeae",
+    justifyContent: "center",
     margin: 10,
   },
-   input_view: {
+  input_view: {
     width: 80,
     height: 80,
-    borderRadius: 80/2,
-    backgroundColor:"#fff",
-    justifyContent:"center",
+    borderRadius: 80 / 2,
+    backgroundColor: "#fff",
+    justifyContent: "center",
     margin: 10,
   },
   input: {
-    color:"#000",
-    textAlign:"center",
-    fontSize:30
-  
+    color: "#000",
+    textAlign: "center",
+    fontSize: 30,
   },
 });
